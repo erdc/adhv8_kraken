fb8f3ad3f5d62b4d07ccff8be6a2a59d0cd55c38
MarkLoveland <markloveland@utexas.edu>
2024-05-13 15:42:48 -0700

    worked on separating routines for time series, bug in new script, need to crosscheck with standalone_mpi.c

--- src/structs_new/xdmf_new/inspecth5.py
+++ src/structs_new/xdmf_new/inspecth5.py
@@ -7,5 +7,10 @@ f1 = h5.File(sys.argv[1],'r')
 print(f1.keys())
 
 
-for a in f1.keys():
-    print(f1[a][:])
+print(f1['Mesh'].keys())
+print(f1['Mesh']['Elements'][:])
+print(f1['Mesh']['XY'][:])
+print(f1['Data'].keys())
+#for a in f1.keys():
+#    print(f1[a].keys())
+

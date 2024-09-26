9bb53d527dccf8b5fe44300c18a7f35c3ed44137
Mark Loveland <markloveland@utexas.edu>
2024-09-26 18:26:19 -0500

    Doxygen working initially, issues with use of macros for equations in html

--- src/NEW_ADH/friction/fr_rheight_to_manningsn.c
+++ src/NEW_ADH/friction/fr_rheight_to_manningsn.c
@@ -1,6 +1,7 @@
-/* ADH Version 2.0.0 6/04 */
-
-/* This routine converts a value of the equivalent sand roughness height to a value of Manning's n.
+/** \file
+ * \brief fr_rheight_to_mannings
+ * 
+ * This file converts a value of the equivalent sand roughness height to a value of Manning's n.
    The conversion is calculated for Metric or English units.
    The input required is:
    double ks
@@ -9,10 +10,15 @@
    double n
 
    Brown Jul 24 2006
- */
+ *  */
+
+
 
 #include "fr_defs.h"
 
+ /** The distance between \f$(x_1,y_1)\f$ and \f$(x_2,y_2)\f$ is 
+  \f$\sqrt{(x_2-x_1)^2+(y_2-y_1)^2}\f$. and \f$ \dwdx \f$
+  */
 double fr_rheight_to_manningsn(
   /* convert roughness height to Manning's n */
   double ks,			/* the equivalent sand roughness height, L */
